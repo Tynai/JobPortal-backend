12fb72daefd495f87f6f202c353307f55451d781
Tynai <bleakk240699@gmail.com>
2022-05-23 11:21:51 +0600

    git ignore

--- server.js
+++ server.js
@@ -11,4 +11,13 @@ app.use('/api/users/', userRoute)
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static('../job-portal-front/build'))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../job-portal-front/build/index.html'))
+  })
+}
+
+
 app.listen(port, () => console.log('Node JS server started'));
